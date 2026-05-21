7769ba071b8968a195e35d9bf3589df3a3419fd7
KenanJtup31 <kelizade349@gmail.com>
2026-05-21 22:51:47 +0400

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- 3. API AÇARI (Bura yeni açarını qoy) ---
-API_KEY = "BURA_YENİ_API_AÇARINI_YAZ"
+API_KEY = "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"
 
 # --- 4. BAŞLIQ (STARK HQ) ---
 st.markdown("""
